76e6acb9ca1fdb0bf99ec769f0cf888ba85a42c4
Hugo <hugovk@users.noreply.github.com>
2018-03-15 08:41:32 +0200

    Add Python 3-only classifier
    https://pypi.python.org/pypi?%3Aaction=list_classifiers

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Quality Assurance',
     ],
