77e6f80a5d28ee3c545c1257ddb6a8f0827fbae5
Łukasz Langa <lukasz@langa.pl>
2018-03-16 00:01:22 -0700

    Bump version, update README with current fixes

--- black.py
+++ black.py
@@ -21,7 +21,7 @@ from blib2to3 import pygram, pytree
 from blib2to3.pgen2 import driver, token
 from blib2to3.pgen2.parse import ParseError
 
-__version__ = "18.3a1"
+__version__ = "18.3a2"
 DEFAULT_LINE_LENGTH = 88
 # types
 syms = pygram.python_symbols
