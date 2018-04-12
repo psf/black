39a5fe7a55d2011d8209888b31fd6a4ea75efb71
Łukasz Langa <lukasz@langa.pl>
2018-04-11 19:22:16 -0700

    18.4a1

--- black.py
+++ black.py
@@ -40,7 +40,7 @@ from blib2to3 import pygram, pytree
 from blib2to3.pgen2 import driver, token
 from blib2to3.pgen2.parse import ParseError
 
-__version__ = "18.4a0"
+__version__ = "18.4a1"
 DEFAULT_LINE_LENGTH = 88
 # types
 syms = pygram.python_symbols
