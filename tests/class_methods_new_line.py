644827d1907949f1bf104091cb81d36fe8fdc36e
Luka Sterbic <luka@fb.com>
2018-05-17 10:37:02 -0700

    Unit test for class formatting looking at new lines between docstrings / vars / methods

---
+++ tests/class_methods_new_line.py
@@ -0,0 +1,42 @@
+class ClassSimplest:
+    pass
+class ClassWithInit:
+    def __init__(self):
+        pass
+class ClassWithInitAndVars:
+    cls_var = 100
+    def __init__(self):
+        pass
+class ClassWithInitAndVarsAndDocstring:
+    """Test class"""
+    cls_var = 100
+    def __init__(self):
+        pass
+
+
+# output
+
+
+class ClassSimplest:
+    pass
+
+
+class ClassWithInit:
+
+    def __init__(self):
+        pass
+
+
+class ClassWithInitAndVars:
+    cls_var = 100
+
+    def __init__(self):
+        pass
+
+
+class ClassWithInitAndVarsAndDocstring:
+    """Test class"""
+    cls_var = 100
+
+    def __init__(self):
+        pass
