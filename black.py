98b6c887da891eb1a4d9401a13ec12f9c5e7f7eb
Łukasz Langa <lukasz@langa.pl>
2018-06-20 12:00:07 -0700

    18.6b3

--- black.py
+++ black.py
@@ -46,7 +46,7 @@ from blib2to3.pgen2 import driver, token
 from blib2to3.pgen2.parse import ParseError
 
 
-__version__ = "18.6b2"
+__version__ = "18.6b3"
 DEFAULT_LINE_LENGTH = 88
 DEFAULT_EXCLUDES = (
     r"/(\.git|\.hg|\.mypy_cache|\.tox|\.venv|_build|buck-out|build|dist)/"
