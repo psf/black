0c5c6098988a436615a76c3a94bff30cc2357dae
Zsolt Dollenstein <zsol.zsol@gmail.com>
2018-08-25 20:02:16 +0100

    make blackd an optional extra

--- setup.py
+++ setup.py
@@ -41,13 +41,8 @@ setup(
     package_data={"blib2to3": ["*.txt"]},
     python_requires=">=3.6",
     zip_safe=False,
-    install_requires=[
-        "click>=6.5",
-        "attrs>=17.4.0",
-        "appdirs",
-        "toml>=0.9.4",
-        "aiohttp>=3.3.2",
-    ],
+    install_requires=["click>=6.5", "attrs>=17.4.0", "appdirs", "toml>=0.9.4"],
+    extras_require={"d": ["aiohttp>=3.3.2"]},
     test_suite="tests.test_black",
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -61,5 +56,5 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Quality Assurance",
     ],
-    entry_points={"console_scripts": ["black=black:main", "blackd=blackd:main"]},
+    entry_points={"console_scripts": ["black=black:main", "blackd=blackd:main [d]"]},
 )
