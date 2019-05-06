2bcb8391c3ebafe9da1972f6d17ce246947465f6
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2019-05-06 15:15:53 -0400

    add parentheses around tuple unpack assignment
    Fixes #656

---
+++ tests/data/tupleassign.py
@@ -0,0 +1,10 @@
+sdfjklsdfsjldkflkjsf, sdfjsdfjlksdljkfsdlkf, sdfsdjfklsdfjlksdljkf, sdsfsdfjskdflsfsdf = 1, 2, 3
+
+
+# output
+(
+    sdfjklsdfsjldkflkjsf,
+    sdfjsdfjlksdljkfsdlkf,
+    sdfsdjfklsdfjlksdljkf,
+    sdsfsdfjskdflsfsdf,
+) = (1, 2, 3)
