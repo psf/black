393073f9e1387565fefa119faa85f0791aaee55f
Carl Meyer <carl@oddbird.net>
2019-05-07 16:39:01 -0400

    Avoid inconsistent formatting when comment follows escaped newline. Fixes #767.

--- black.py
+++ black.py
@@ -2127,15 +2127,21 @@ def list_comments(prefix: str, *, is_endmarker: bool) -> List[ProtoComment]:
 
     consumed = 0
     nlines = 0
+    ignored_lines = 0
     for index, line in enumerate(prefix.split("\n")):
         consumed += len(line) + 1  # adding the length of the split '\n'
         line = line.lstrip()
         if not line:
             nlines += 1
         if not line.startswith("#"):
+            # Escaped newlines outside of a comment are not really newlines at
+            # all. We treat a single-line comment following an escaped newline
+            # as a simple trailing comment.
+            if line.endswith("\\"):
+                ignored_lines += 1
             continue
 
-        if index == 0 and not is_endmarker:
+        if index == ignored_lines and not is_endmarker:
             comment_type = token.COMMENT  # simple trailing comment
         else:
             comment_type = STANDALONE_COMMENT
---
+++ tests/data/comment_after_escaped_newline.py
@@ -0,0 +1,8 @@
+def bob(): \
+         # pylint: disable=W9016
+    pass
+
+# output
+
+def bob():  # pylint: disable=W9016
+    pass
