b73ec93fa71ae8980d0743555a9f49f46528e6e1
Joe Antonakakis <jma353@cornell.edu>
2019-10-20 07:16:26 -0700

    Add .svn to default exclusion list (#965)

--- black.py
+++ black.py
@@ -54,9 +54,7 @@ from blib2to3.pgen2.parse import ParseError
 from _version import version as __version__
 
 DEFAULT_LINE_LENGTH = 88
-DEFAULT_EXCLUDES = (
-    r"/(\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|_build|buck-out|build|dist)/"
-)
+DEFAULT_EXCLUDES = r"/(\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|\.svn|_build|buck-out|build|dist)/"  # noqa: B950
 DEFAULT_INCLUDES = r"\.pyi?$"
 CACHE_DIR = Path(user_cache_dir("black", version=__version__))
 
