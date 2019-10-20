4b5852e44ac9d2d2aa10fbbfa66a5f02421e7768
Yurii Karabas <1998uriyyo@gmail.com>
2019-10-20 17:52:07 +0300

    Set correct return statement for `is_type_comment` function (#929)

--- black.py
+++ black.py
@@ -2777,7 +2777,7 @@ def is_type_comment(leaf: Leaf, suffix: str = "") -> bool:
     Only returns true for type comments for now."""
     t = leaf.type
     v = leaf.value
-    return t in {token.COMMENT, t == STANDALONE_COMMENT} and v.startswith(
+    return t in {token.COMMENT, STANDALONE_COMMENT} and v.startswith(
         "# type:" + suffix
     )
 
