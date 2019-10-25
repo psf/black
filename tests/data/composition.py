9ebb89bfde38b3213118f8b5db105141321ee845
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2019-10-25 08:11:30 -0700

    fix tests

--- tests/data/composition.py
+++ tests/data/composition.py
@@ -166,14 +166,17 @@ class C:
             _C.__init__.__code__.co_firstlineno + 1,
         )
 
-        assert expectedexpectedexpectedexpectedexpectedexpectedexpectedexpectedexpect == {
-            key1: value1,
-            key2: value2,
-            key3: value3,
-            key4: value4,
-            key5: value5,
-            key6: value6,
-            key7: value7,
-            key8: value8,
-            key9: value9,
-        }
+        assert (
+            expectedexpectedexpectedexpectedexpectedexpectedexpectedexpectedexpect
+            == {
+                key1: value1,
+                key2: value2,
+                key3: value3,
+                key4: value4,
+                key5: value5,
+                key6: value6,
+                key7: value7,
+                key8: value8,
+                key9: value9,
+            }
+        )
