95a7c3ade0aec0ac8143e80c2f41b557bc5206e6
Vlad Emelianov <volshebnyi@gmail.com>
2019-10-30 13:30:31 +0100

    Use testlist_star_expr in return_stmt grammar

---
+++ tests/data/python38.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.8
+
+
+def starred_return():
+    my_list = ["test", "test2"]
+    return "asd", *my_list
+
+
+# output
+
+
+#!/usr/bin/env python3.8
+
+
+def starred_return():
+    my_list = ["test", "test2"]
+    return "asd", *my_list
