2758bab616e46039234b9cf4e0c1fe96a638b50c
Vlad Emelianov <volshebnyi@gmail.com>
2019-11-20 05:53:52 +0100

    Remove line_str argument from is_line_short_enough

--- black.py
+++ black.py
@@ -4220,7 +4220,7 @@ def enumerate_with_length(
         yield index, leaf, length
 
 
-def is_line_short_enough(line: Line, *, line_length: int, line_str: str = "") -> bool:
+def is_line_short_enough(line: Line, line_length: int) -> bool:
     """Return True if `line` is no longer than `line_length`.
 
     Uses the provided `line_str` rendering, if any, otherwise computes a new one.
