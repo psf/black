d76912e63193ada212a86543b06a4bb84e89d90b
Bryan Bugyi <bryanbugyi34@gmail.com>
2020-02-08 19:03:00 -0500

    [misc] Removed debugging code for troubleshooting pipeline bug

--- black.py
+++ black.py
@@ -1308,16 +1308,8 @@ class Line:
                 leaf, complex_subscript=self.is_complex_subscript(leaf)
             )
         if self.inside_brackets or not preformatted:
-            try:
-                self.bracket_tracker.mark(leaf)
-                self.maybe_remove_trailing_comma(leaf)
-            except KeyError:
-                err(
-                    "Aborting line append due to a problem with the BracketTracker."
-                    " The problemed Line, as it looks now, is shown"
-                    f" below:\n\n{line_to_string(self)}\n"
-                )
-                raise
+            self.bracket_tracker.mark(leaf)
+            self.maybe_remove_trailing_comma(leaf)
         if not self.append_comment(leaf):
             self.leaves.append(leaf)
 
@@ -2606,13 +2598,6 @@ def split_line(
                 )
         except CannotSplit:
             continue
-        except Exception as e:
-            err(
-                f"{e.__class__.__name__} raised while attempting to split the"
-                f" following line using the {split_func}"
-                f" splitter:\n\n{line_to_string(line)}\n"
-            )
-            raise
         else:
             yield from result
             break
