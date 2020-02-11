71fb99a893b28ad1dc41d76170351d02d09fd028
Bryan Bugyi <bryanbugyi34@gmail.com>
2020-02-10 19:10:15 -0500

    [doc] Add 'Examples' section to _insert_str_child_factory(...) docstring

--- black.py
+++ black.py
@@ -3349,6 +3349,45 @@ class StringSplitterMixin(StringTransformerMixin):
         Factory for a convenience function that is used to orphan @string_leaf
         and then insert multiple new leaves into the same part of the node
         structure that @string_leaf had originally occupied.
+
+        Examples:
+            Let `N = string_leaf.parent` and assume the node `N` has the
+            following original structure:
+
+            Node(
+                expr_stmt, [
+                    Leaf(NAME, 'x'),
+                    Leaf(EQUAL, '='),
+                    Leaf(STRING, '"foo"'),
+                ]
+            )
+
+            We then run the code snippet shown below.
+            ```
+            insert_str_child = self._insert_str_child_factory(string_leaf)
+
+            lpar = Leaf(token.LPAR, '(')
+            insert_str_child(lpar)
+
+            bar = Leaf(token.STRING, '"bar"')
+            insert_str_child(bar)
+
+            rpar = Leaf(token.RPAR, ')')
+            insert_str_child(rpar)
+            ```
+
+            After which point, it follows that `string_leaf.parent is None` and
+            the node `N` now has the following structure:
+
+            Node(
+                expr_stmt, [
+                    Leaf(NAME, 'x'),
+                    Leaf(EQUAL, '='),
+                    Leaf(LPAR, '('),
+                    Leaf(STRING, '"bar"'),
+                    Leaf(RPAR, ')'),
+                ]
+            )
         """
         string_parent = string_leaf.parent
         string_child_idx = string_leaf.remove()
@@ -3366,8 +3405,9 @@ class StringSplitterMixin(StringTransformerMixin):
 
     def __validate(self, line: Line, string_idx: int) -> STResult[None]:
         """
-        Checks that @line meets all of the requirements listed in this Mixin's
-        docstring.
+        Checks that @line meets all of the requirements listed in this classes'
+        docstring. Refer to `help(StringSplitterMixin)` for a detailed
+        description of those requirements.
 
         Returns:
             * Ok(None), if ALL of the requirements are met.
