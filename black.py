9c24fbc7e3b65b7967c2ffd4a4e2f272df655645
Bryan Bugyi <bryanbugyi34@gmail.com>
2020-02-29 11:45:55 -0500

    [rem] Remove DEBUG variable

--- black.py
+++ black.py
@@ -59,7 +59,6 @@ from blib2to3.pgen2.parse import ParseError
 
 from _black_version import version as __version__
 
-DEBUG = False
 DEFAULT_LINE_LENGTH = 88
 DEFAULT_EXCLUDES = r"/(\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|\.svn|_build|buck-out|build|dist)/"  # noqa: B950
 DEFAULT_INCLUDES = r"\.pyi?$"
@@ -2459,9 +2458,6 @@ def transform_line(
         return
 
     line_str = line_to_string(line)
-    if DEBUG:
-        print(repr(line.leaves[0].parent))
-        print()
 
     def init_st(ST: Type[StringTransformer]) -> StringTransformer:
         """Initialize StringTransformer"""
@@ -3065,7 +3061,7 @@ class StringParensStripper(StringTransformer):
 
                 return Ok(string_idx)
 
-        return TErr(f"This line has no strings wrapped in parens.")
+        return TErr("This line has no strings wrapped in parens.")
 
     def do_transform(self, line: Line, string_idx: int) -> Iterator[TResult[Line]]:
         LL = line.leaves
