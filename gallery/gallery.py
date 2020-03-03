d6e29ba86b4a462ed5717bba4b3873d1fab6f65d
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2020-03-03 20:42:49 +0300

    Ability to run formatter, parse black versions, commit

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -1,26 +1,45 @@
 import json
+import subprocess
+import sys
 import tarfile
 import tempfile
 import zipfile
-import subprocess
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from typing import Union
+from typing import NamedTuple, Optional, Union
 from urllib.request import urlopen, urlretrieve
 
+GIT_AUTHOR = "black/gallery <>"
 PYPI_INSTANCE = "https://pypi.org/pypi"
+
 ArchiveKind = Union[tarfile.TarFile, zipfile.ZipFile]
+
+
+class BlackVersion(NamedTuple):
+    version: str
+    pyproject_toml: Optional[str] = None
 
 
 def get_pypi_download_url(package: str, version: Optional[str]) -> str:
     with urlopen(PYPI_INSTANCE + f"/{package}/json") as page:
         metadata = json.load(page)
 
-    for source in metadata["urls"]:
+    if version is None:
+        sources = metadata["urls"]
+    else:
+        if version in metadata["releases"]:
+            sources = metadata["releases"][version]
+        else:
+            raise ValueError(
+                f"No releases found with given version ('{version}') tag. Releases: {metadata['releases'].keys()}"
+            )
+
+    for source in sources:
         if source["python_version"] == "source":
             break
     else:
         raise ValueError(f"Couldn't find any sources for {package}")
+
     return source["url"]
 
 
@@ -55,8 +74,8 @@ def get_first_archive_member(archive: ArchiveKind) -> str:
         return archive.namelist()[0]
 
 
-def download_and_extract(package: str, directory: Path) -> Path:
-    source = get_package_source(package)
+def download_and_extract(package: str, version: Optional[str], directory: Path) -> Path:
+    source = get_package_source(package, version)
 
     local_file, _ = urlretrieve(source, directory / f"{package}-src")
     with get_archive_manager(local_file) as archive:
@@ -65,25 +84,82 @@ def download_and_extract(package: str, directory: Path) -> Path:
     return directory / result_dir
 
 
-def create_git_repository(directory: Path) -> None:
+def git_create_repository(directory: Path) -> None:
     subprocess.run(["git", "init"], cwd=directory)
+    git_add_and_commit(msg="Inital commit", repo=directory)
+
+
+def git_add_and_commit(msg: str, repo: Path) -> None:
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", msg, "--author", GIT_AUTHOR])
+
+
+def git_switch_branch(branch: str, repo: Path, new=False) -> None:
+    args = ["git", "checkout"]
+    if new:
+        args.append("-b")
+    args.append(branch)
+    subprocess.run(args, cwd=repo)
+
+
+def init_repo(options: Namespace) -> Path:
+    options.output.mkdir(exist_ok=True)
+
+    source_directory = download_and_extract(
+        package=options.pypi_package, version=options.version, directory=options.output
+    )
+    git_create_repository(source_directory)
+    return source_directory
+
+
+def format_repo_with_version(
+    repo: Path, black_version: BlackVersion, black_repo: Path
+) -> None:
+    git_switch_branch(black_version.version, repo=black_repo)
+    git_switch_branch(f"black-{black_version.version}", repo=black_repo, new=True)
+    formatter = [sys.executable, (black_repo / "black.py").resolve(), "."]
+    if black_version.pyproject_toml:
+        formatter.extend(["--config", black_version.pyproject_toml])
+    subprocess.run(formatter, cwd=repo)
+    subprocess.run(
+        ["git", "commit", "-m", "Format with black", "--author", "Black Gallery <>"],
+        cwd=repo,
+    )
+
+
+def format_repo(repo: Path, options: Namespace) -> None:
+    black_versions = (BlackVersion(*version.split(":")) for version in options.versions)
+
+    for black_version in black_versions:
+        format_repo_with_version(
+            repo=repo, black_version=black_version, black_repo=options.black_repo,
+        )
+    git_switch_branch("master", repo=repo)
 
 
 def main() -> None:
     parser = ArgumentParser()
-    parser.add_argument("-p", "--pypi-package", help="PyPI package to download")
+
+    parser.add_argument(
+        "-p", "--pypi-package", help="PyPI package to download", required=True
+    )
+    parser.add_argument(
+        "-b", "--black-repo", help="Black's git repository", type=Path, required=True
+    )
+    parser.add_argument(
+        "-v", "--version", help="Version for PyPI package", default=None
+    )
     parser.add_argument(
         "-o",
         "--output",
         default=Path("output/"),
         help="Output directory to download packages",
     )
+    parser.add_argument("versions", nargs="*", default=("master",), help="")
 
     options = parser.parse_args()
-    options.output.mkdir(exist_ok=True)
-
-    source_directory = download_and_extract(options.pypi_package, options.output)
-    create_git_repository(source_directory)
+    repo = init_repo(options)
+    format_repo(repo, options)
 
 
 if __name__ == "__main__":
