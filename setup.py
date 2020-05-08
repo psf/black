6661b0528b25e39d0b4491c2ec88b1ef1808ee09
Cooper Lees <me@cooperlees.com>
2020-05-08 06:29:06 -0700

    Update setup.py
    Co-authored-by: Hugo van Kemenade <hugovk@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     author="Łukasz Langa",
     author_email="lukasz@langa.pl",
     url="https://github.com/psf/black",
-    project_urls={"Change Log": "https://github.com/psf/black/blob/master/CHANGES.md"},
+    project_urls={"Changelog": "https://github.com/psf/black/blob/master/CHANGES.md"},
     license="MIT",
     py_modules=["black", "blackd", "_black_version"],
     ext_modules=ext_modules,
