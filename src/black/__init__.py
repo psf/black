21842f22573d8da956faae01835fbf6e6ea26ae0
James Addison <jay@jp-hosting.net>
2021-02-03 01:02:23 +0000

    Fixup: handling of invisible parentheses for assert and return statements

--- src/black/__init__.py
+++ src/black/__init__.py
@@ -5381,6 +5381,9 @@ def normalize_invisible_parens(node: Node, parens_after: Set[str]) -> None:
                     node.insert_child(index, Leaf(token.LPAR, ""))
                     node.append_child(Leaf(token.RPAR, ""))
                 break
+            elif node.type in [syms.assert_stmt, syms.return_stmt]:
+                if maybe_make_parens_invisible_in_atom(child, parent=node):
+                    wrap_in_parentheses(node, child, visible=False)
 
             elif not (isinstance(child, Leaf) and is_multiline_string(child)):
                 wrap_in_parentheses(node, child, visible=False)
