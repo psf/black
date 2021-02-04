21528480a85f26c7b8192cb18892be309fe9d31c
James Addison <jay@jp-hosting.net>
2021-02-04 13:55:41 +0000

    Add additional test cases from psf/black#1629
    These are generally cases where example code was provided by a user and it was straightforward to copy and confirm the stability test failure for that code

--- tests/data/invisible_parens_instability.py
+++ tests/data/invisible_parens_instability.py
@@ -1,1 +1,57 @@
+# https://github.com/psf/black/issues/1629#issuecomment-681953135
+assert (
+    xxxxxx(
+        xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+        xxxxxxxxxxxxxxxxxxxxxxxxx
+    )
+    == xxxxxx(xxxxxxxxxxx, xxxxxxxxxxxxxxxxxxxxxxxxx)
+)
+
+
+# https://github.com/psf/black/issues/1629#issuecomment-688804670
+if any(
+    k in t
+    for k in ["AAAAAAAAAA", "AAAAA", "AAAAAA", "AAAAAAAAA", "AAA", "AAAAAA", "AAAAAAAA", "AAA", "AAAAA", "AAAAA", "AAAA"]
+) and not any(k in t for k in ["AAA"]):
+    pass
+
+
+# https://github.com/psf/black/issues/1629#issuecomment-693816208
+aaaaaaaaaaaaaaaaaaaaaaaaaa = bbbbbbbbbbbbbbbbbbbb(  # ccccccccccccccccccccccccccccccccccc
+    d=0
+)
+
+
+# https://github.com/psf/black/issues/1629#issuecomment-699006156
+def f():
+    return a(
+        b(
+          c(n)
+         #
+         ),
+        []
+     ) + d(x, y)
+
+
+# https://github.com/psf/black/issues/1629#issuecomment-704782983
+def test_foo():
+    assert foo(
+        "foo",
+    ) == [{"raw": {"person": "1"}, "error": "Invalid field unknown", "status": "error"}]
+
+
+# https://github.com/psf/black/issues/1629#issuecomment-707066734
+my_string = '______________ %s _________________ %s _____________________________________________' % (
+    '____________', '________________', '___________________________________________')
+
+
+# https://github.com/psf/black/issues/1629#issuecomment-764562642
+class Foo(object):
+    def bar(self):
+        x = 'foo ' + \
+            'subscription {} resource_group {} Site {}, foobar_name {}. Error {}'.format(
+                self._subscription, self._resource_group, self._site, self._foobar_name, error)
+
+
+# https://github.com/psf/black/issues/1629#issuecomment-772691120
 assert function(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15) != [None]
