16bf728b6749c3b73709303415c7c857c059ab78
James Addison <jay@jp-hosting.net>
2021-02-04 14:06:28 +0000

    Revert "Update output expectation for trailing comma reformatting test case"
    This reverts commit d262d295baf7ec91e2a7cb6a9ce1bf7621b51b1f.

--- tests/data/function_trailing_comma.py
+++ tests/data/function_trailing_comma.py
@@ -23,7 +23,6 @@ def xxxxxxxxxxxxxxxxxxxxxxxxxxxx() -> Set[
 
 # output
 
-
 def f(
     a,
 ):
@@ -62,19 +61,16 @@ def f(
         "a": 1,
         "b": 2,
     }["a"]
-    if (
-        a
-        == {
-            "a": 1,
-            "b": 2,
-            "c": 3,
-            "d": 4,
-            "e": 5,
-            "f": 6,
-            "g": 7,
-            "h": 8,
-        }["a"]
-    ):
+    if a == {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+    }["a"]:
         pass
 
 
