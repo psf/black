b2a4a291e3c6c70f0d428853d73a21b0e5adfca3
James Addison <jay@jp-hosting.net>
2021-02-09 21:20:43 +0000

    Include explanatory comments in 'rhs' method from 586d24236e6b57bc3b5da85fdbe2563835021076

--- src/black/__init__.py
+++ src/black/__init__.py
@@ -2722,10 +2722,19 @@ def transform_line(
     else:
 
         def rhs(line: Line, features: Collection[Feature]) -> Iterator[Line]:
+            """Wraps calls to `right_hand_split`.
+            The calls increasingly `omit` right-hand trailers (bracket pairs with
+            content), meaning the trailers get glued together to split on another
+            bracket pair instead.
+            """
             for omit in generate_trailers_to_omit(line, mode.line_length):
                 lines = list(
                     right_hand_split(line, mode.line_length, features, omit=omit)
                 )
+                # Note: this check is only able to figure out if the first line of the
+                # *current* transformation fits in the line length.  This is true only
+                # for simple cases.  All others require running more transforms via
+                # `transform_line()`.  This check doesn't know if those would succeed.
                 if is_line_short_enough(lines[0], line_length=mode.line_length):
                     yield from lines
                     return
