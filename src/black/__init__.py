b06158931331b9144a4cc33ee292d271063d449b
James Addison <jay@jp-hosting.net>
2021-02-13 11:56:45 +0000

    Remove special-case handling for 'assert' and 'if' statements

--- src/black/__init__.py
+++ src/black/__init__.py
@@ -5387,8 +5387,6 @@ def normalize_invisible_parens(node: Node, parens_after: Set[str]) -> None:
                     node.insert_child(index, Leaf(token.LPAR, ""))
                     node.append_child(Leaf(token.RPAR, ""))
                 break
-            elif node.type in {syms.assert_stmt, syms.if_stmt}:
-                pass
 
             elif not (isinstance(child, Leaf) and is_multiline_string(child)):
                 wrap_in_parentheses(node, child, visible=False)
