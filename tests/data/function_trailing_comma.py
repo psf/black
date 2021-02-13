eab9b2b38d91cccf6ea1a7270b02f7466a295969
James Addison <jay@jp-hosting.net>
2021-02-13 12:36:01 +0000

    Remove outdated comment in test data

--- tests/data/function_trailing_comma.py
+++ tests/data/function_trailing_comma.py
@@ -17,7 +17,6 @@ def f(a:int=1,):
         pass
     if a == {"a": 1,"b": 2,"c": 3,"d": 4,"e": 5,"f": 6,"g": 7,"h": 8,}:
         pass
-    # TODO: Why didn't 'while' require special normalize_invisible_parens handling, like assert/return?
     while a in {"a": 1,"b": 2,"c": 3,"d": 4,"e": 5,"f": 6,"g": 7,"h": 8,}:
         pass
     assert val[-1] in (
@@ -92,7 +91,6 @@ def f(
         "h": 8,
     }:
         pass
-    # TODO: Why didn't 'while' require special normalize_invisible_parens handling, like assert/return?
     while a in {
         "a": 1,
         "b": 2,
