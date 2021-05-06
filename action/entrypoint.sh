df27360fff0d5074bf6d71f04882819ab28aad74
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-05-06 14:47:18 -0400

    Fix broken Action entrypoint

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-if [ -n $INPUT_BLACK_ARGS ]; then
+if [ -n "$INPUT_BLACK_ARGS" ]; then
   echo '::warning::Input `with.black_args` is deprecated. Use `with.options` and `with.src` instead.'
   black $INPUT_BLACK_ARGS
   exit $?
+fi
 
 black $INPUT_OPTIONS $INPUT_SRC
