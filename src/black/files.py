c8d894f3863281e9182b059056c09aaab842d02b
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-05-15 01:22:25 +0300

    Fix: no parent .gitignore

--- src/black/files.py
+++ src/black/files.py
@@ -212,7 +212,7 @@ def gen_python_files(
                 extend_exclude,
                 force_exclude,
                 report,
-                gitignore + get_gitignore(child) if gitignore else None,
+                gitignore + get_gitignore(child) if gitignore is not None else None,
             )
 
         elif child.is_file():
