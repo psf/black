4d9565b37facc4daa97d663d74fb6a81bcdb2aa0
Sergey Vartanov <me@enzet.ru>
2021-06-03 01:12:06 +0300

    Add output to test

--- tests/data/fmtskip6.py
+++ tests/data/fmtskip6.py
@@ -3,3 +3,11 @@ class A:
         for line in range(10):
             if True:
                 pass  # fmt: skip
+
+# output
+
+class A:
+    def f(self):
+        for line in range(10):
+            if True:
+                pass  # fmt: skip
