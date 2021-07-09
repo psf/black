e45a2087e1ee7994570c774e424c7053e1de564e
MarcoGorelli <marcogorelli@protonmail.com>
2021-07-09 18:30:07 +0100

    windows fixup

--- tests/test_no_ipynb.py
+++ tests/test_no_ipynb.py
@@ -9,7 +9,7 @@ pytestmark = pytest.mark.no_jupyter
 
 def test_ipynb_diff_with_no_change() -> None:
     path = THIS_DIR / "data/notebook_trailing_newline.ipynb"
-    msg = f"Skipping '{path}' as IPython is not installed."
+    msg = r"Skipping '.*' as IPython is not installed."
     with pytest.warns(UserWarning, match=msg):
         format_file_in_place(
             path, fast=False, mode=DEFAULT_MODE, write_back=WriteBack.DIFF
