138bad4bca58d01bc87aeb30471cd46b555943a4
MarcoGorelli <marcogorelli@protonmail.com>
2021-08-12 14:16:26 +0100

    use git tag

--- scripts/check_pre_commit_rev_in_example.py
+++ scripts/check_pre_commit_rev_in_example.py
@@ -1,32 +1,35 @@
 import os
+import subprocess
 
 import commonmark
 import yaml
 from bs4 import BeautifulSoup
 
 
-def main(changes: str, source_version_control: str) -> None:
-    html = commonmark.commonmark(changes)
-    soup = BeautifulSoup(html, "html.parser")
-    latest_version = [
-        header.string for header in soup.find_all("h2") if header.string != "Unreleased"
-    ][0]
+def main(source_version_control: str) -> None:
+    latest_tag = subprocess.run(
+        ["git", "describe", "--abbrev=0", "--tags"],
+        universal_newlines=True,
+        stdout=subprocess.PIPE,
+    ).stdout.rstrip()
 
     html = commonmark.commonmark(source_version_control)
     soup = BeautifulSoup(html, "html.parser")
     pre_commit_repos = yaml.safe_load(soup.find(class_="language-yaml").string)["repos"]
-    if not all(repo["rev"] == latest_version for repo in pre_commit_repos):
-        raise ValueError(
-            "Please set the rev in ``source_version_control.md`` to be the latest one\n"
-        )
+
+    for repo in pre_commit_repos:
+        pre_commit_rev = repo["rev"]
+        if not pre_commit_rev == latest_tag:
+            raise ValueError(
+                "Please set the rev in ``source_version_control.md`` to be the latest "
+                f"one.\nExpected {latest_tag}, got {pre_commit_rev}\n"
+            )
 
 
 if __name__ == "__main__":
-    with open("CHANGES.md", encoding="utf-8") as fd:
-        changes = fd.read()
     with open(
         os.path.join("docs", "integrations", "source_version_control.md"),
         encoding="utf-8",
     ) as fd:
         source_version_control = fd.read()
-    main(changes, source_version_control)
+    main(source_version_control)
