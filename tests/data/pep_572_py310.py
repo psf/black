7336634e1a726ea486b34bc871ee1c1641255333
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-08-26 16:39:58 -0400

    Add additional test on 3.9 and fix some typos

--- tests/data/pep_572_py310.py
+++ tests/data/pep_572_py310.py
@@ -1,4 +1,4 @@
-# Unparenthesized warluses are now allowed in indices since Python 3.10.
+# Unparenthesized walruses are now allowed in indices since Python 3.10.
 x[a:=0]
 x[a:=0, b:=1]
 x[5, b:=0]
--- tests/data/pep_572_py39.py
+++ tests/data/pep_572_py39.py
@@ -1,4 +1,7 @@
-# Unparenthesized warluses are now allowed in set lterals & set comprehensions
+# Unparenthesized walruses are now allowed in set literals & set comprehensions
 # since Python 3.9
 {x := 1, 2, 3}
 {x4 := x ** 5 for x in range(7)}
+# We better not remove the parentheses here (since it's a 3.10 feature)
+x[(a := 1)]
+x[(a := 1), (b := 3)]
