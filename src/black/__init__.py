dbf3e556bc70df92ecab4f0a7277e5c8d057d5d3
Daniel Sparing <dsparing@google.com>
2021-11-19 06:57:23 +0000

    minor formatting

--- src/black/__init__.py
+++ src/black/__init__.py
@@ -944,7 +944,8 @@ def format_file_contents(src_contents: str, *, fast: bool, mode: Mode) -> FileCo
 
 
 def validate_cell(src: str) -> None:
-    """Check that cell does not already contain TransformerManager transformations, or cell magics, which might cause TransformerManager to break.
+    """Check that cell does not already contain TransformerManager transformations,
+    or cell magics, which might cause TransformerManager to break.
 
     If a cell contains ``!ls``, then it'll be transformed to
     ``get_ipython().system('ls')``. However, if the cell originally contained
@@ -960,8 +961,8 @@ def validate_cell(src: str) -> None:
     """
     if any(transformed_magic in src for transformed_magic in TRANSFORMED_MAGICS):
         raise NothingChanged
-    elif any('%%' + cell_magic in src for cell_magic in NON_PYTHON_CELL_MAGICS):
-        raise NothingChanged        
+    if any("%%" + cell_magic in src for cell_magic in NON_PYTHON_CELL_MAGICS):
+        raise NothingChanged
 
 
 def format_cell(src: str, *, fast: bool, mode: Mode) -> str:
