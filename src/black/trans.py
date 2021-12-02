458c874b81cb0de731060832cacf00b7acede175
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-12-01 19:14:34 -0800

    actually remove it

--- src/black/trans.py
+++ src/black/trans.py
@@ -4,7 +4,7 @@ String transformers that can split and merge strings.
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from dataclasses import dataclass
-import regex as re  # We need recursive patterns here (?R)
+import re
 from typing import (
     Any,
     Callable,
