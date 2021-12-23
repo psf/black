cb3ec99926e143c8d36079e4442db6a184949f47
Richard Si <63936253+ichard26@users.noreply.github.com>
2021-12-23 16:34:01 -0500

    Add diff-shades integration
    Hopefully this makes it much easier to gauge the impacts of future
    changes!

--- docs/contributing/gauging_changes.md
+++ docs/contributing/gauging_changes.md
@@ -40,3 +40,56 @@ If you're running locally yourself to test black on lots of code try:
 ```{program-output} black-primer --help
 
 ```
+
+## diff-shades
+
+diff-shades is a tool similar to black-primer, it also runs _Black_ across a list of Git
+cloneable OSS projects recording the results. The intention is to eventually fully
+replace black-primer with diff-shades as it's much more feature complete and supports
+our needs better.
+
+The main highlight feature of diff-shades is being able to compare two revisions of
+_Black_. This is incredibly useful as it allows us to see what exact changes will occur,
+say merging a certain PR. Black-primer's results would usually be filled with changes
+caused by pre-existing code in Black drowning out the (new) changes we want to see. It
+operates similarly to black-primer but crucially it saves the results as a JSON file
+which allows for the rich comparison features alluded to above.
+
+For more information, please see the [diff-shades documentation][diff-shades].
+
+### CI integration
+
+diff-shades is also the tool behind the "diff-shades results comparing ..." /
+"diff-shades reports zero changes ..." comments on PRs. The project has a GitHub Actions
+workflow which runs diff-shades twice against two revisions of _Black_ according to
+these rules:
+
+|                       | Baseline revision       | Target revision              |
+| --------------------- | ----------------------- | ---------------------------- |
+| On PRs                | latest commit on `main` | PR commit with `main` merged |
+| On pushes (main only) | latest PyPI version     | the pushed commit            |
+
+Once finished, a PR comment will posted embedding a summary of the changes and links to
+further information. If there's a pre-existing diff-shades comment, it'll be updated
+instead the next time the workflow is triggered on the same PR.
+
+The workflow uploads 3-4 artifacts upon completion: the two generated analyses (they
+have the .json file extension), `diff.html`, and `.pr-comment-body.md` if triggered by a
+PR. The last one is downloaded by the `diff-shades-comment` workflow and shouldn't be
+downloaded locally. `diff.html` comes in handy for push-based or manually triggered
+runs. And the analyses exist just in case you want to do further analysis using the
+collected data locally.
+
+Note that the workflow will only fail intentionally if while analyzing a file failed to
+format. Otherwise it's mostly likely a bug of the workflow.
+
+```{tip}
+Maintainers with write access or higher can trigger the workflow manually from the
+Actions tab using the `workflow_dispatch` event. Simply select "diff-shades"
+from the workflows list on the left, press "Run workflow", and fill in which revisions
+and command line arguments to use.
+
+Once finished, check the logs or download the artifacts for local use.
+```
+
+[diff-shades]: https://github.com/ichard26/diff-shades#readme
---
+++ scripts/diff_shades_gha_helper.py
@@ -0,0 +1,262 @@
+import json
+import os
+import platform
+import pprint
+import subprocess
+import sys
+import zipfile
+from io import BytesIO
+from pathlib import Path
+from typing import Any, Dict, Optional, Tuple
+
+import click
+import urllib3
+from packaging.version import Version
+
+if sys.version_info >= (3, 8):
+    from typing import Final, Literal
+else:
+    from typing_extensions import Final, Literal
+
+COMMENT_BODY_FILE: Final = ".pr-comment-body.md"
+DOCS_URL: Final = (
+    "https://black.readthedocs.io/en/latest/"
+    "contributing/gauging_changes.html#diff-shades"
+)
+SHA_LENGTH: Final = 10
+GH_API_TOKEN: Final = os.getenv("GITHUB_TOKEN")
+REPO: Final = os.getenv("GITHUB_REPOSITORY", default="psf/black")
+http = urllib3.PoolManager()
+
+
+def set_output(name: str, value: str) -> None:
+    if len(value) < 200:
+        print(f"[INFO]: setting '{name}' to '{value}'")
+    else:
+        print(f"[INFO]: setting '{name}' to [{len(value)} chars]")
+    print(f"::set-output name={name}::{value}")
+
+
+def http_get(
+    url: str,
+    is_json: bool = True,
+    headers: Optional[Dict[str, str]] = None,
+    **kwargs: Any,
+) -> Any:
+    headers = headers or {}
+    if GH_API_TOKEN:
+        headers["Authorization"] = f"token {GH_API_TOKEN}"
+    if "github" in url:
+        headers["Accept"] = "application/vnd.github.v3+json"
+    headers["User-Agent"] = "psf/black diff-shades workflow via urllib3"
+    r = http.request("GET", url, headers=headers, **kwargs)
+    if is_json:
+        data = json.loads(r.data.decode("utf-8"))
+    else:
+        data = r.data
+    print(f"[INFO]: issued GET request for {r.geturl()}")
+    if not (200 <= r.status < 300):
+        pprint.pprint(dict(r.info()))
+        pprint.pprint(data)
+        raise RuntimeError(f"unexpected status code: {r.status}")
+
+    return data
+
+
+def get_branch_or_tag_revision(sha: str = "main") -> str:
+    data = http_get(
+        f"https://api.github.com/repos/{REPO}/commits",
+        fields={"per_page": "1", "sha": sha},
+    )
+    assert isinstance(data[0]["sha"], str)
+    return data[0]["sha"]
+
+
+def get_pr_revision(pr: int) -> str:
+    data = http_get(f"https://api.github.com/repos/{REPO}/pulls/{pr}")
+    assert isinstance(data["head"]["sha"], str)
+    return data["head"]["sha"]
+
+
+def get_pypi_version() -> Version:
+    data = http_get("https://pypi.org/pypi/black/json")
+    versions = [Version(v) for v in data["releases"]]
+    sorted_versions = sorted(versions, reverse=True)
+    return sorted_versions[0]
+
+
+def resolve_custom_ref(ref: str) -> Tuple[str, str]:
+    if ref == ".pypi":
+        # Special value to get latest PyPI version.
+        version = str(get_pypi_version())
+        return version, f"git checkout {version}"
+
+    if ref.startswith(".") and ref[1:].isnumeric():
+        # Special format to get a PR.
+        number = int(ref[1:])
+        revision = get_pr_revision(number)
+        return f"pr-{number}-{revision[:SHA_LENGTH]}", f"gh pr checkout {number}"
+
+    # Alright, it's probably a branch, tag, or a commit SHA, let's find out!
+    revision = get_branch_or_tag_revision(ref)
+    # We're cutting the revision short as we might be operating on a short commit SHA.
+    if revision == ref or revision[: len(ref)] == ref:
+        # It's *probably* a commit as the resolved SHA isn't different from the REF.
+        return revision[:SHA_LENGTH], f"git checkout {revision}"
+
+    # It's *probably* a pre-existing branch or tag, yay!
+    return f"{ref}-{revision[:SHA_LENGTH]}", f"git checkout {revision}"
+
+
+@click.group()
+def main() -> None:
+    pass
+
+
+@main.command("config", help="Acquire run configuration and metadata.")
+@click.argument(
+    "event", type=click.Choice(["push", "pull_request", "workflow_dispatch"])
+)
+@click.argument("custom_baseline", required=False)
+@click.argument("custom_target", required=False)
+def config(
+    event: Literal["push", "pull_request", "workflow_dispatch"],
+    custom_baseline: Optional[str],
+    custom_target: Optional[str],
+) -> None:
+    if event == "push":
+        # Push on main, let's use PyPI Black as the baseline.
+        baseline_name = str(get_pypi_version())
+        baseline_cmd = f"git checkout {baseline_name}"
+        target_rev = os.getenv("GITHUB_SHA")
+        assert target_rev is not None
+        target_name = "main-" + target_rev[:SHA_LENGTH]
+        target_cmd = f"git checkout {target_rev}"
+
+    elif event == "pull_request":
+        # PR, let's use main as the baseline.
+        baseline_rev = get_branch_or_tag_revision()
+        assert baseline_rev is not None, "main should exist ..."
+        baseline_name = "main-" + baseline_rev[:SHA_LENGTH]
+        baseline_cmd = f"git checkout {baseline_rev}"
+
+        pr_ref = os.getenv("GITHUB_REF")
+        assert pr_ref is not None
+        pr_num = int(pr_ref[10:-6])
+        pr_rev = get_pr_revision(pr_num)
+        target_name = f"pr-{pr_num}-{pr_rev[:SHA_LENGTH]}"
+        target_cmd = f"gh pr checkout {pr_num} && git merge origin/main"
+
+        # These are only needed for the PR comment.
+        set_output("baseline-sha", baseline_rev)
+        set_output("target-sha", pr_rev)
+    else:
+        assert custom_baseline is not None and custom_target is not None
+        baseline_name, baseline_cmd = resolve_custom_ref(custom_baseline)
+        target_name, target_cmd = resolve_custom_ref(custom_target)
+        if baseline_name == target_name:
+            # Alright we're using the same revisions but we're (hopefully) using
+            # different command line arguments, let's support that too.
+            baseline_name += "-1"
+            target_name += "-2"
+
+    set_output("baseline-analysis", baseline_name + ".json")
+    set_output("baseline-setup-cmd", baseline_cmd)
+    set_output("target-analysis", target_name + ".json")
+    set_output("target-setup-cmd", target_cmd)
+
+
+@main.command("cache-key", help="Generate a cache-key for an analysis.")
+@click.argument("type", type=click.Choice(["baseline", "target"]))
+@click.argument("name")
+@click.argument("black-args", nargs=-1, type=click.UNPROCESSED)
+def cache_key(
+    type: Literal["baseline", "target"], name: str, black_args: Tuple[str, ...]
+) -> None:
+    import diff_shades
+
+    args_digest = "".join(black_args).encode("utf-8").hex()
+    key = f"{platform.system()}-{platform.python_version()}-{diff_shades.__version__}"
+    key += f"-{name}-{args_digest}"
+    set_output(f"{type}-cache-key", key)
+
+
+@main.command("comment-body", help="Generate the body for a summary PR comment.")
+@click.argument("baseline", type=click.Path(exists=True, path_type=Path))
+@click.argument("target", type=click.Path(exists=True, path_type=Path))
+@click.argument("baseline-sha")
+@click.argument("target-sha")
+def comment_body(
+    baseline: Path, target: Path, baseline_sha: str, target_sha: str
+) -> None:
+    # fmt: off
+    cmd = [
+        sys.executable, "-m", "diff_shades", "--no-color",
+        "compare", str(baseline), str(target), "--quiet", "--check"
+    ]
+    # fmt: on
+    proc = subprocess.run(cmd, stdout=subprocess.PIPE, encoding="utf-8")
+    if not proc.returncode:
+        body = (
+            f"**diff-shades** reports zero changes comparing this PR ({target_sha}) to"
+            f" main ({baseline_sha}).\n\n---\n\n"
+        )
+    else:
+        body = (
+            f"**diff-shades** results comparing this PR ({target_sha}) to main"
+            f" ({baseline_sha}). The full diff is [available in the logs]"
+            '($job-diff-url) under the "Generate HTML diff report" step.'
+        )
+        body += "\n```text\n" + proc.stdout.strip() + "\n```\n"
+    body += (
+        f"[**What is this?**]({DOCS_URL}) | [Workflow run]($workflow-run-url) |"
+        " [diff-shades documentation](https://github.com/ichard26/diff-shades#readme) |"
+        # This is used by the comment workflow to discover a pre-existing comment.
+        " id: diff-shades-comment"
+    )
+    print(f"[INFO]: writing half-completed comment body to {COMMENT_BODY_FILE}")
+    with open(COMMENT_BODY_FILE, "w", encoding="utf-8") as f:
+        f.write(body)
+
+
+@main.command(
+    "comment-details", help="Get comment details from an analysis workflow run."
+)
+@click.argument("run-id")
+def comment_details(run_id: str) -> None:
+    data = http_get(f"https://api.github.com/repos/{REPO}/actions/runs/{run_id}")
+    if data["event"] != "pull_request":
+        set_output("needs-comment", "false")
+        return
+
+    set_output("needs-comment", "true")
+    pulls = data["pull_requests"]
+    assert len(pulls) == 1
+    pr_number = pulls[0]["number"]
+    set_output("pr-number", str(pr_number))
+
+    jobs_data = http_get(data["jobs_url"])
+    job = jobs_data["jobs"][0]
+    steps = {s["name"]: s["number"] for s in job["steps"]}
+    diff_step = steps["Generate HTML diff report"]
+    diff_url = job["html_url"] + f"#step:{diff_step}:1"
+
+    artifacts_data = http_get(data["artifacts_url"])["artifacts"]
+    artifacts = {a["name"]: a["archive_download_url"] for a in artifacts_data}
+    body_url = artifacts[COMMENT_BODY_FILE]
+    body_zip = BytesIO(http_get(body_url, is_json=False))
+    with zipfile.ZipFile(body_zip) as zfile:
+        with zfile.open(COMMENT_BODY_FILE) as rf:
+            body = rf.read().decode("utf-8")
+    # It's more convenient to fill these fields after the first workflow is done
+    # since this command can access the workflows API.
+    body = body.replace("$workflow-run-url", data["html_url"])
+    body = body.replace("$job-diff-url", diff_url)
+
+    print(f"[INFO]: writing comment body to {COMMENT_BODY_FILE}")
+    with open(COMMENT_BODY_FILE, "w", encoding="utf-8") as wf:
+        wf.write(body)
+
+
+if __name__ == "__main__":
+    main()
