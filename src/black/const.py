31c962ed234ae2889e74b5f4885c7643d6fd9cda
Shivansh-007 <shivansh-007@outlook.com>
2022-01-21 12:09:06 +0530

    Use correct typehint for `LogLevel.none`

--- src/black/const.py
+++ src/black/const.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Any
+from typing_extensions import TypedDict
 
 DEFAULT_LINE_LENGTH = 88
 DEFAULT_EXCLUDES = r"/(\.direnv|\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|venv|\.svn|_build|buck-out|build|dist)/"  # noqa: B950
@@ -7,8 +7,12 @@ DEFAULT_INCLUDES = r"(\.pyi?|\.ipynb)$"
 STDIN_PLACEHOLDER = "__BLACK_STDIN_FILENAME__"
 
 
+class Empty(TypedDict):
+    pass
+
+
 class LogLevel(Enum):
-    none: Any = dict()
+    none: Empty = {}
     trace = dict(fg="cyan", dim=True)
     debug = dict(fg="green", dim=True)
     info = dict(fg="blue")
