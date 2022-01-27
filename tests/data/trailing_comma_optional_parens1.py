b92822afeedd45daa3b1d094a502daf936f7fa9d
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-26 19:44:39 -0800

    more trailing comma tests (#2810)

--- tests/data/trailing_comma_optional_parens1.py
+++ tests/data/trailing_comma_optional_parens1.py
@@ -2,6 +2,25 @@ if e1234123412341234.winerror not in (_winapi.ERROR_SEM_TIMEOUT,
                         _winapi.ERROR_PIPE_BUSY) or _check_timeout(t):
     pass
 
+
+class X:
+    def get_help_text(self):
+        return ngettext(
+            "Your password must contain at least %(min_length)d character.",
+            "Your password must contain at least %(min_length)d characters.",
+            self.min_length,
+        ) % {'min_length': self.min_length}
+
+class A:
+    def b(self):
+        if self.connection.mysql_is_mariadb and (
+            10,
+            4,
+            3,
+        ) < self.connection.mysql_version < (10, 5, 2):
+            pass
+
+
 # output
 
 if (
@@ -13,3 +32,30 @@ if (
     or _check_timeout(t)
 ):
     pass
+
+
+class X:
+    def get_help_text(self):
+        return (
+            ngettext(
+                "Your password must contain at least %(min_length)d character.",
+                "Your password must contain at least %(min_length)d characters.",
+                self.min_length,
+            )
+            % {"min_length": self.min_length}
+        )
+
+
+class A:
+    def b(self):
+        if (
+            self.connection.mysql_is_mariadb
+            and (
+                10,
+                4,
+                3,
+            )
+            < self.connection.mysql_version
+            < (10, 5, 2)
+        ):
+            pass
