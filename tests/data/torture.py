1e77ae42a2b1d2cbb0a83beeeab9a5a4268b6645
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-28 17:08:45 -0800

    test fixes

--- tests/data/torture.py
+++ tests/data/torture.py
@@ -31,20 +31,17 @@ importA
     ** 101234234242352525425252352352525234890264906820496920680926538059059209922523523525
 )  #
 
-assert (
-    sort_by_dependency(
-        {
-            "1": {"2", "3"},
-            "2": {"2a", "2b"},
-            "3": {"3a", "3b"},
-            "2a": set(),
-            "2b": set(),
-            "3a": set(),
-            "3b": set(),
-        }
-    )
-    == ["2a", "2b", "2", "3a", "3b", "3", "1"]
-)
+assert sort_by_dependency(
+    {
+        "1": {"2", "3"},
+        "2": {"2a", "2b"},
+        "3": {"3a", "3b"},
+        "2a": set(),
+        "2b": set(),
+        "3a": set(),
+        "3b": set(),
+    }
+) == ["2a", "2b", "2", "3a", "3b", "3", "1"]
 
 importA
 0
@@ -54,8 +51,8 @@ importA
 class A:
     def foo(self):
         for _ in range(10):
-            aaaaaaaaaaaaaaaaaaa = bbbbbbbbbbbbbbb.cccccccccc(
-                xxxxxxxxxxxx
+            aaaaaaaaaaaaaaaaaaa = (
+                bbbbbbbbbbbbbbb.cccccccccc(xxxxxxxxxxxx)
             )  # pylint: disable=no-member
 
 
