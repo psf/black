ce9e47d72712781f55025f32a514317ad31adb9d
Shivansh-007 <shivansh-007@outlook.com>
2022-03-14 08:30:25 +0530

    Remove walrus operator expressions

--- tests/data/paren_eq_check_in_assigments.py
+++ tests/data/paren_eq_check_in_assigments.py
@@ -1,8 +1,5 @@
 match_count += new_value == old_value
 
-if on_windows := os.name == "nt":
-    ...
-
 on_windows: bool = (os.name == "nt")
 
 implementation_version = (
@@ -19,13 +16,6 @@ name1 == name2 == name3
 
 check_sockets(on_windows=os.name == "nt")
 
-if a := b == c:
-    pass
-
-a = [y := f(x) == True, y ** 2, y ** 3]
-
-a = lambda line: (m := re.match(pattern, line) == True)
-
 a = b in c and b == d
 
 a = b == c == d
@@ -41,9 +31,6 @@ a = b > c
 # output
 
 match_count += (new_value == old_value)
-
-if on_windows := (os.name == "nt"):
-    ...
 
 on_windows: bool = (os.name == "nt")
 
@@ -62,13 +49,6 @@ name1 = name2 = name3
 name1 == name2 == name3
 
 check_sockets(on_windows=os.name == "nt")
-
-if a := (b == c):
-    pass
-
-a = [y := (f(x) == True), y**2, y**3]
-
-a = lambda line: (m := (re.match(pattern, line) == True))
 
 a = b in c and b == d
 
