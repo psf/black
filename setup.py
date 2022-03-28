3f8a9b6e3ddc7f7603833773a034748d4c484fa8
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-03-28 14:07:23 -0400

    Pin click between 8.0.0 and 8.0.4 as 8.1.0 drops Python 3.6

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
     python_requires=">=3.6.2",
     zip_safe=False,
     install_requires=[
-        "click>=8.0.0",
+        "click>=8.0.0, <=8.0.4",
         "platformdirs>=2",
         "tomli>=1.1.0; python_version < '3.11'",
         "typed-ast>=1.4.2; python_version < '3.8' and implementation_name == 'cpython'",
