c207ba687980760a4cbb7ea7ba0258213385ca5a
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-06-10 20:43:42 -0400

    Update documentation dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
