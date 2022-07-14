39317cda959f7bbe06ae6bfd18ed93248a7f9c16
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-07-14 15:30:09 -0700

    Update src/black/linegen.py

--- src/black/linegen.py
+++ src/black/linegen.py
@@ -295,7 +295,7 @@ class LineGenerator(Visitor[Line]):
             # indentation of those changes the AST representation of the code.
             if Preview.normalize_docstring_quotes_and_prefixes_properly in self.mode:
                 # There was a bug where --skip-string-normalization wouldn't stop us
-                # from normalize docstring prefixes. To maintain stability, we can only
+                # from normalizing docstring prefixes. To maintain stability, we can only
                 # address this buggy behaviour while the preview style is enabled.
                 if self.mode.string_normalization:
                     docstring = normalize_string_prefix(leaf.value)
