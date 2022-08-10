566efb58385d5cc2fd2fb836b515fbf488e71133
hauntsaninja <hauntsaninja@gmail.com>
2022-08-09 20:47:32 -0700

    comment

--- tests/test_black.py
+++ tests/test_black.py
@@ -321,6 +321,7 @@ class BlackTestCase(BlackBaseTestCase):
         features = black.get_features_used(root)
         self.assertNotIn(black.Feature.DEBUG_F_STRINGS, features)
 
+        # We don't yet support feature version detection in nested f-strings
         root = black.lib2to3_parse(
             """f"heard a rumour that { f'{1+1=}' } ... seems like it could be true" """
         )
