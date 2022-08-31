2c90480e1a102ab0fac57737d2ba5143d82abed7
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2022-08-30 20:46:46 -0700

    Use strict mypy checking (#3222)
    Co-authored-by: Richard Si <63936253+ichard26@users.noreply.github.com>

--- src/blackd/middlewares.py
+++ src/blackd/middlewares.py
@@ -9,7 +9,7 @@ Middleware = Callable[[Request, Handler], Awaitable[StreamResponse]]
 
 
 def cors(allow_headers: Iterable[str]) -> Middleware:
-    @middleware
+    @middleware  # type: ignore[misc]
     async def impl(request: Request, handler: Handler) -> StreamResponse:
         is_options = request.method == "OPTIONS"
         is_preflight = is_options and "Access-Control-Request-Method" in request.headers
@@ -32,4 +32,4 @@ def cors(allow_headers: Iterable[str]) -> Middleware:
 
         return resp
 
-    return impl  # type: ignore
+    return impl  # type: ignore[no-any-return]
