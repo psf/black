68750a3b06b234578333e6c752d33d4fd4264373
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-08-31 15:25:20 -0400

    The external role is actually a Sphinx 4.4+ feature

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ make_pypi_svg(release)
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = "3.0"
+needs_sphinx = "4.4"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
