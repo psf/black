062e644aae4299a320aeac59085df4c020ba6c81
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-09-05 16:27:05 -0400

    Mitigate deprecation of aiohttp's `@middleware` decorator (#3259)
    This is deprecated since aiohttp 4.0. If it doesn't exist just define a
    no-op decorator that does nothing (after the other aiohttp imports
    though!). By doing this, it's safe to ignore the DeprecationWarning
    without needing to require the latest aiohttp once they remove
    `@middleware`.

--- src/blackd/middlewares.py
+++ src/blackd/middlewares.py
@@ -1,15 +1,25 @@
-from typing import Awaitable, Callable, Iterable
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Iterable, TypeVar
 
-from aiohttp.web_middlewares import middleware
 from aiohttp.web_request import Request
 from aiohttp.web_response import StreamResponse
+
+if TYPE_CHECKING:
+    F = TypeVar("F", bound=Callable[..., Any])
+    middleware: Callable[[F], F]
+else:
+    try:
+        from aiohttp.web_middlewares import middleware
+    except ImportError:
+        # @middleware is deprecated and its behaviour is the default since aiohttp 4.0
+        # so if it doesn't exist anymore, define a no-op for forward compatibility.
+        middleware = lambda x: x  # noqa: E731
 
 Handler = Callable[[Request], Awaitable[StreamResponse]]
 Middleware = Callable[[Request, Handler], Awaitable[StreamResponse]]
 
 
 def cors(allow_headers: Iterable[str]) -> Middleware:
-    @middleware  # type: ignore[misc]
+    @middleware
     async def impl(request: Request, handler: Handler) -> StreamResponse:
         is_options = request.method == "OPTIONS"
         is_preflight = is_options and "Access-Control-Request-Method" in request.headers
@@ -32,4 +42,4 @@ def cors(allow_headers: Iterable[str]) -> Middleware:
 
         return resp
 
-    return impl  # type: ignore[no-any-return]
+    return impl
