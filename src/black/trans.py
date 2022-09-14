e2adcd7de10eb570987bb894d95f2ff8c8693b9f
Yilei "Dolee" Yang <yileiyang@google.com>
2022-09-13 20:23:51 -0700

    Fix a crash on dicts with paren-wrapped long string keys (#3262)
    Fix a crash when formatting some dicts with parenthesis-wrapped long
    string keys. When LL[0] is an atom string, we need to check the atom
    node's siblings instead of LL[0] itself, e.g.:
    
        dictsetmaker
          atom
            STRING '"This is a really long string that can\'t be expected to fit in one line and is used as a nested dict\'s key"'
          /atom
          COLON ':'
          atom
            LSQB ' ' '['
            listmaker
              STRING '"value"'
              COMMA ','
              STRING ' ' '"value"'
            /listmaker
            RSQB ']'
          /atom
          COMMA ','
        /dictsetmaker

--- src/black/trans.py
+++ src/black/trans.py
@@ -1071,6 +1071,16 @@ class BaseStringSplitter(StringTransformer):
             # And the string is surrounded by commas (or is the first/last child)...
             prev_sibling = LL[0].prev_sibling
             next_sibling = LL[0].next_sibling
+            if (
+                not prev_sibling
+                and not next_sibling
+                and parent_type(LL[0]) == syms.atom
+            ):
+                # If it's an atom string, we need to check the parent atom's siblings.
+                parent = LL[0].parent
+                assert parent is not None  # For type checkers.
+                prev_sibling = parent.prev_sibling
+                next_sibling = parent.next_sibling
             if (not prev_sibling or prev_sibling.type == token.COMMA) and (
                 not next_sibling or next_sibling.type == token.COMMA
             ):
