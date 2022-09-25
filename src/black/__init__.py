d3bef04b6c18832ba24af2fae53550fd7268878c
Richard Si <63936253+ichard26@users.noreply.github.com>
2022-09-25 17:03:29 -0400

    Add comment

--- src/black/__init__.py
+++ src/black/__init__.py
@@ -1375,6 +1375,8 @@ def patch_click() -> None:
 
 
 def patched_main() -> None:
+    # PyInstaller patches multiprocessing to need freeze_support() even in non-Windows
+    # environments so just assume we always need to call it if frozen.
     if getattr(sys, "frozen", False):
         from multiprocessing import freeze_support
 
