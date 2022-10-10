3c71bd64806dce37fa64b2afacaefff72c9e9d29
Yilei Yang <yileiyang@google.com>
2022-10-10 10:20:48 -0700

    Format code.

--- tests/test_blackd.py
+++ tests/test_blackd.py
@@ -23,7 +23,6 @@ else:
     try:
         from aiohttp.test_utils import unittest_run_loop
     except ImportError:
-
         # unittest_run_loop is unnecessary and a no-op since aiohttp 3.8, and
         # aiohttp 4 removed it. To maintain compatibility we can make our own
         # no-op decorator.
