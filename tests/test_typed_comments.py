0cca1d794c2dcfaa0b783a80284fa59dbb44d7e2
Itxaso Aizpurua <itsaso23a@gmail.com>
2022-10-20 22:21:29 +0200

    lint

--- tests/test_typed_comments.py
+++ tests/test_typed_comments.py
@@ -6,13 +6,8 @@ from blib2to3.pytree import Leaf
 
 
 @pytest.mark.parametrize(
-    "comment_str", [
-        "# type: int",
-        "#  type: int",
-        "#   type: int",
-        "# type : int",
-        "# type  : int"
-    ]
+    "comment_str",
+    ["# type: int", "#  type: int", "#   type: int", "# type : int", "# type  : int"],
 )
 def test_typed_comments(comment_str: str) -> None:
     assert is_type_comment(Leaf(token.COMMENT, comment_str))
