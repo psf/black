92370825cede7ac7901d64405842226c8d710653
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-12-10 08:46:51 -0800

    normalize_docstring_quotes_and_prefixes_properly (unused)

--- src/black/mode.py
+++ src/black/mode.py
@@ -150,7 +150,6 @@ class Preview(Enum):
     """Individual preview style features."""
 
     long_docstring_quotes_on_newline = auto()
-    normalize_docstring_quotes_and_prefixes_properly = auto()
     one_element_subscript = auto()
     remove_block_trailing_newline = auto()
     remove_redundant_parens = auto()
