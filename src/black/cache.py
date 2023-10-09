92ec66e53ef36677443f0f3e14da7a5809f425a0
hauntsaninja <hauntsaninja@gmail.com>
2023-10-09 13:47:00 -0700

    fix return any

--- src/black/cache.py
+++ src/black/cache.py
@@ -37,8 +37,8 @@ def get_cache_dir() -> Path:
     """
     # NOTE: Function mostly exists as a clean way to test getting the cache directory.
     default_cache_dir = user_cache_dir("black")
-    cache_dir = Path(os.environ.get("BLACK_CACHE_DIR", default_cache_dir))
-    return cache_dir / __version__
+    cache_dir = Path(os.environ.get("BLACK_CACHE_DIR", default_cache_dir)) / __version__
+    return cache_dir
 
 
 CACHE_DIR = get_cache_dir()
