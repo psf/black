0727532701c7455731bd9b84c8494cf75a3b3cda
Charles Patel <charles.patel@apkudo.com>
2023-10-09 15:09:33 -0500

    Ignore mypy errors for scripts.* and add aiohttp in mypy extra deps

--- src/blackd/__init__.py
+++ src/blackd/__init__.py
@@ -74,7 +74,7 @@ def main(bind_host: str, bind_port: int) -> None:
     app = make_app()
     ver = black.__version__
     black.out(f"blackd version {ver} listening on {bind_host} port {bind_port}")
-    web.run_app(app, host=bind_host, port=bind_port, handle_signals=True, print=None)
+    web.run_app(app, host=bind_host, port=bind_port, handle_signals=True, print=None)  # type: ignore[arg-type]
 
 
 def make_app() -> web.Application:
