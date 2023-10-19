ffde64d7f1d3c147a7eae079aa566707c80c33d8
Henri Holopainen <henri.i.holopainen@gmail.com>
2023-10-19 14:30:47 +0300

    Don't merge lines with multiline strings with inline comments

--- src/black/lines.py
+++ src/black/lines.py
@@ -239,6 +239,21 @@ class Line:
 
         return False
 
+    def contains_implicit_multiline_string_with_comments(self) -> bool:
+        """Chck if we have an implicit multiline string with comments on the line"""
+        for leaf_type, leaf_group_iterator in itertools.groupby(
+            self.leaves, lambda leaf: leaf.type
+        ):
+            if leaf_type != token.STRING:
+                continue
+            leaf_list = list(leaf_group_iterator)
+            if len(leaf_list) == 1:
+                continue
+            for leaf in leaf_list:
+                if self.comments_after(leaf):
+                    return True
+        return False
+
     def contains_uncollapsable_type_comments(self) -> bool:
         ignored_ids = set()
         try:
