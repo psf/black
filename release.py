b16d8f122fb044a88845583cc2bd71b9924d2f5e
Cooper Ry Lees <me@cooperlees.com>
2023-10-23 20:22:47 -0700

    Add release tool
    - Add tool for release managers to use to generate commits
      - I'm trying to only use stdlib so we have no depdencies ...
    - Default is to change date strings in hard coded documentation files + CHANGES.md
      - I write directly to files cause we have SCM to fix any screw ups ...
    - We hackily convert calver to ints to sort (all for better ideas here)
      - If we hit a ValueError we just set to 0 for sorting - This is alhpa + beta release we can safely ignore these days
    - Add new CI to only run release unittests in 3.12 only on all platforms
    - Update release docs
    
    - Checked with `mypy --strict` + ensure we are `black --preview` formatted :D
    
    Tests:
    - Run it to generate template PR
      - `python3.12 release.py --debug --add-changes-template`
    - Run it to cleanup CHANGE.md + change version in specified doc files
    ```
    crl-m1:black cooper$ python3.12 release.py -d
    [2023-10-23 23:39:38,414] INFO: Current version detected to be 23.10.1 (release.py:221)
    [2023-10-23 23:39:38,414] INFO: Next version will be 23.10.2 (release.py:222)
    [2023-10-23 23:39:38,414] INFO: Cleaning up /Users/cooper/repos/black/CHANGES.md (release.py:127)
    [2023-10-23 23:39:38,416] DEBUG: Finished Cleaning up /Users/cooper/repos/black/CHANGES.md (release.py:147)
    [2023-10-23 23:39:38,416] INFO: Updating black version to 23.10.2 in /Users/cooper/repos/black/docs/integrations/source_version_control.md (release.py:173)
    [2023-10-23 23:39:38,416] DEBUG: Finished updating black version to 23.10.2 in /Users/cooper/repos/black/docs/integrations/source_version_control.md (release.py:185)
    [2023-10-23 23:39:38,416] INFO: Updating black version to 23.10.2 in /Users/cooper/repos/black/docs/usage_and_configuration/the_basics.md (release.py:173)
    [2023-10-23 23:39:38,417] DEBUG: Finished updating black version to 23.10.2 in /Users/cooper/repos/black/docs/usage_and_configuration/the_basics.md (release.py:185)
    ```
    - Add tests around some key logic

---
+++ release.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+"""
+Tool to help automate changes needed in commits during and after releases
+"""
+
+import argparse
+import logging
+import sys
+from datetime import datetime
+from pathlib import Path
+from subprocess import PIPE, run
+from typing import List
+
+
+LOG = logging.getLogger(__name__)
+NEW_VERSION_CHANGELOG_TEMPLATE = """\
+## Unreleased
+
+### Highlights
+
+<!-- Include any especially major or disruptive changes here -->
+
+### Stable style
+
+<!-- Changes that affect Black's stable style -->
+
+### Preview style
+
+<!-- Changes that affect Black's preview style -->
+
+### Configuration
+
+<!-- Changes to how Black can be configured -->
+
+### Packaging
+
+<!-- Changes to how Black is packaged, such as dependency requirements -->
+
+### Parser
+
+<!-- Changes to the parser or to version autodetection -->
+
+### Performance
+
+<!-- Changes that improve Black's performance. -->
+
+### Output
+
+<!-- Changes to Black's terminal output and error messages -->
+
+### _Blackd_
+
+<!-- Changes to blackd -->
+
+### Integrations
+
+<!-- For example, Docker, GitHub Actions, pre-commit, editors -->
+
+### Documentation
+
+<!-- Major changes to documentation and policies. Small docs changes
+     don't need a changelog entry. -->
+"""
+
+
+# TODO: Do better with alpha + beta releases
+# Maybe we vendor packaging library
+def get_git_tags(versions_only: bool = True) -> List[str]:
+    """Pull out all tags or calvers only"""
+    git_tags = run(
+        ["git", "tag"], stdout=PIPE, check=True, encoding="utf8"
+    ).stdout.splitlines()
+    if versions_only:
+        return [t for t in git_tags if t[0].isdigit()]
+    return git_tags
+
+
+def int_calver(calver: str) -> int:
+    """Convert a calver string into an hex base 16 integer for sorting
+    - So we can support hexadecimal chars"""
+    # Return 0 for all alpha + beta releases as we can ignore them
+    try:
+        return int(calver.capitalize().replace(".", ""))
+    except ValueError:
+        return 0
+
+
+class SourceFiles:
+    def __init__(self, black_repo_dir: Path):
+        # File path fun all pathlib to be platform agnostic
+        self.black_repo_path = black_repo_dir
+        self.changes_path = self.black_repo_path / "CHANGES.md"
+        self.docs_path = self.black_repo_path / "docs"
+        self.version_doc_paths = (
+            self.docs_path / "integrations" / "source_version_control.md",
+            self.docs_path / "usage_and_configuration" / "the_basics.md",
+        )
+        self.current_version = self.get_current_version()
+        self.next_version = self.get_next_version()
+
+    def add_template_to_changes(self) -> int:
+        """Add the template to CHANGES.md if it does not exist"""
+        LOG.info(f"Adding template to {self.changes_path}")
+
+        with self.changes_path.open("r") as cfp:
+            changes_string = cfp.read()
+
+        if "## Unreleased" in changes_string:
+            LOG.error(f"{self.changes_path} already has unreleased template")
+            return 1
+
+        templated_changes_string = changes_string.replace(
+            "# Change Log\n",
+            f"# Change Log\n\n{NEW_VERSION_CHANGELOG_TEMPLATE}",
+        )
+
+        with self.changes_path.open("w") as cfp:
+            cfp.write(templated_changes_string)
+
+        LOG.info(f"Added template to {self.changes_path}")
+        return 0
+
+    def cleanup_changes_template_for_release(self) -> None:
+        LOG.info(f"Cleaning up {self.changes_path}")
+
+        with self.changes_path.open("r") as cfp:
+            changes_string = cfp.read()
+
+        # Change Unreleased to next version
+        versioned_changes = changes_string.replace(
+            "## Unreleased", f"## {self.next_version}"
+        )
+
+        # Remove all comments (subheadings are harder - Human will still need to do them)
+        no_comments_changes = []
+        for line in versioned_changes.splitlines():
+            if line.startswith("<!--"):
+                continue
+            no_comments_changes.append(line)
+
+        with self.changes_path.open("w") as cfp:
+            cfp.write("\n".join(no_comments_changes) + "\n")
+
+        LOG.debug(f"Finished Cleaning up {self.changes_path}")
+
+    def get_current_version(self) -> str:
+        """Get the latest git (version) tag as latest version"""
+        return sorted(get_git_tags(), key=lambda k: int_calver(k))[-1]
+
+    def get_next_version(self) -> str:
+        """Workout the year and month + version number we need to move to"""
+        base_calver = datetime.today().strftime("%y.%m")
+        calver_parts = base_calver.split(".")
+        base_calver = f"{calver_parts[0]}.{int(calver_parts[1])}"  # Remove leading 0
+        git_tags = get_git_tags()
+        same_month_releases = [t for t in git_tags if t.startswith(base_calver)]
+        if len(same_month_releases) < 1:
+            return f"{base_calver}.0"
+        same_month_version = same_month_releases[-1].split(".", 2)[-1]
+        return f"{base_calver}.{int(same_month_version) + 1}"
+
+    def update_repo_for_release(self) -> int:
+        """Update CHANGES.md + doc files ready for release"""
+        self.cleanup_changes_template_for_release()
+        self.update_version_in_docs()
+        return 0  # return 0 if no exceptions hit
+
+    def update_version_in_docs(self) -> None:
+        for doc_path in self.version_doc_paths:
+            LOG.info(f"Updating black version to {self.next_version} in {doc_path}")
+
+            with doc_path.open("r") as dfp:
+                doc_string = dfp.read()
+
+            next_version_doc = doc_string.replace(
+                self.current_version, self.next_version
+            )
+
+            with doc_path.open("w") as dfp:
+                dfp.write(next_version_doc)
+
+            LOG.debug(
+                f"Finished updating black version to {self.next_version} in {doc_path}"
+            )
+
+
+def _handle_debug(debug: bool) -> None:
+    """Turn on debugging if asked otherwise INFO default"""
+    log_level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)s: %(message)s (%(filename)s:%(lineno)d)",
+        level=log_level,
+    )
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-a",
+        "--add-changes-template",
+        action="store_true",
+        help="Add the Unreleased template to CHANGES.md",
+    )
+    parser.add_argument(
+        "-d", "--debug", action="store_true", help="Verbose debug output"
+    )
+    args = parser.parse_args()
+    _handle_debug(args.debug)
+    return args
+
+
+def main() -> int:
+    args = parse_args()
+
+    sf = SourceFiles(Path(__file__).parent)
+
+    if args.add_changes_template:
+        return sf.add_template_to_changes()
+
+    LOG.info(f"Current version detected to be {sf.current_version}")
+    LOG.info(f"Next version will be {sf.next_version}")
+    return sf.update_repo_for_release()
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(main())
---
+++ release_tests.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import unittest
+from pathlib import Path
+from shutil import copy, rmtree
+from tempfile import TemporaryDirectory
+from typing import Any
+from unittest.mock import Mock, patch
+
+from release import SourceFiles, int_calver
+
+
+class FakeDateTime:
+    """Used to mock the date to test generating next calver function"""
+
+    def today(*args: Any, **kwargs: Any) -> "FakeDateTime":
+        return FakeDateTime()
+
+    # Add leading 0 on purpose to ensure we remove it
+    def strftime(*args: Any, **kwargs: Any) -> str:
+        return "69.01"
+
+
+class TestRelease(unittest.TestCase):
+    def setUp(self) -> None:
+        self.tempdir = TemporaryDirectory(delete=False)
+        self.tempdir_path = Path(self.tempdir.name)
+        self.sf_real_black_repo = SourceFiles(Path(__file__).parent)
+        self.sf = SourceFiles(self.tempdir_path)
+        self._make_fake_black_repo()
+        # check we have a repo + changes file
+        self.assertTrue(self.sf.changes_path.exists())
+
+    def _make_fake_black_repo(self) -> None:
+        copy(self.sf_real_black_repo.changes_path, self.sf.changes_path)
+        for idx, doc_path in enumerate(self.sf.version_doc_paths):
+            doc_path.parent.mkdir(parents=True, exist_ok=True)
+            copy(self.sf_real_black_repo.version_doc_paths[idx], doc_path)
+
+    def tearDown(self) -> None:
+        rmtree(self.tempdir.name)
+        return super().tearDown()
+
+    @patch("release.get_git_tags")
+    def test_get_current_version(self, mocked_git_tags: Mock) -> None:
+        mocked_git_tags.return_value = ["1.1.0", "69.1.0", "69.1.1", "2.2.0"]
+        self.assertEqual("69.1.1", self.sf.get_current_version())
+
+    @patch("release.get_git_tags")
+    @patch("release.datetime", FakeDateTime)
+    def test_get_next_version(self, mocked_git_tags: Mock) -> None:
+        # test we handle no args
+        mocked_git_tags.return_value = []
+        self.assertEqual(
+            "69.1.0",
+            self.sf.get_next_version(),
+            "Unable to get correct next version with no git tags",
+        )
+
+        # test we handle
+        mocked_git_tags.return_value = ["1.1.0", "69.1.0", "69.1.1", "2.2.0"]
+        self.assertEqual(
+            "69.1.2",
+            self.sf.get_next_version(),
+            "Unable to get correct version with 2 previous versions released this"
+            " month",
+        )
+
+    def test_int_calver(self) -> None:
+        first_month_release = int_calver("69.1.0")
+        second_month_release = int_calver("69.1.1")
+        self.assertEqual(6910, first_month_release)
+        self.assertEqual(6911, second_month_release)
+        self.assertTrue(first_month_release < second_month_release)
+
+
+if __name__ == "__main__":
+    unittest.main()
