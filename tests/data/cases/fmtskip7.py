71ca016ca662deb670145305b547e5558798952f
Henri Holopainen <henri.i.holopainen@gmail.com>
2023-10-26 17:55:20 +0300

    Add more test cases

--- tests/data/cases/fmtskip7.py
+++ tests/data/cases/fmtskip7.py
@@ -3,9 +3,15 @@ b =     5  #fmt:skip
 c = 9  #fmt: skip
 d = "thisisasuperlongstringthisisasuperlongstringthisisasuperlongstringthisisasuperlongstring"  #fmt:skip
 
+if True: print("yay")  # fmt: skip
+class Foo: ...  # fmt: skip
+
 # output
 
 a = "this is some code"
 b =     5  # fmt:skip
 c = 9  # fmt: skip
 d = "thisisasuperlongstringthisisasuperlongstringthisisasuperlongstringthisisasuperlongstring"  # fmt:skip
+
+if True: print("yay")  # fmt: skip
+class Foo: ...  # fmt: skip
