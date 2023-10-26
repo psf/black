31b68e1ee3990408b700b4039d2e099141ede698
Henri Holopainen <henri.i.holopainen@gmail.com>
2023-10-26 11:29:05 +0300

    Add new test case

--- tests/data/cases/fmtskip5.py
+++ tests/data/cases/fmtskip5.py
@@ -8,12 +8,30 @@ if (
 else:
     print("I'm bad")
 
+if (
+    a ==    3  # fmt: skip
+    and b    != 9  # fmt: skip
+    and c is not None
+):
+    print("I'm good!")
+else:
+    print("I'm bad")
+
 
 # output
 
 a, b, c = 3, 4, 5
 if (
     a == 3
+    and b    != 9  # fmt: skip
+    and c is not None
+):
+    print("I'm good!")
+else:
+    print("I'm bad")
+
+if (
+    a ==    3  # fmt: skip
     and b    != 9  # fmt: skip
     and c is not None
 ):
