1a6206d769191852b5800269622e4a21b1a32a45
Yilei Yang <yileiyang@google.com>
2023-11-08 20:53:13 -0800

    Oops, fix the test case after last minute new comment.

--- tests/data/cases/line_ranges_diff_edge_case.py
+++ tests/data/cases/line_ranges_diff_edge_case.py
@@ -1,4 +1,4 @@
-# flags: --line-ranges=8-9
+# flags: --line-ranges=10-11
 # NOTE: If you need to modify this file, pay special attention to the --line-ranges=
 # flag above as it's formatting specifically these lines.
 
@@ -13,11 +13,11 @@ print ( "format me" )
 print ( "format me" )
 
 # output
-# flags: --line-ranges=8-9
+# flags: --line-ranges=10-11
 # NOTE: If you need to modify this file, pay special attention to the --line-ranges=
 # flag above as it's formatting specifically these lines.
 
-# Reproducible example for https://github.com/psf/black/issues/4033
+# Reproducible example for https://github.com/psf/black/issues/4033.
 # This can be fixed in the future if we use a better diffing algorithm, or make Black
 # perform formatting in a single pass.
 
