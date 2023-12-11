3cad66368b6b19e285f642410d6e13eb3023a8f4
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2023-12-11 13:39:36 -0800

    Fix another dummy impl case

--- src/black/nodes.py
+++ src/black/nodes.py
@@ -736,15 +736,18 @@ def is_funcdef(node: Node) -> bool:
     return node.type == syms.funcdef
 
 
+def is_function_or_class(node: Node) -> bool:
+    return node.type in {syms.funcdef, syms.classdef, syms.async_funcdef}
+
+
 def is_stub_suite(node: Node, mode: Mode) -> bool:
     """Return True if `node` is a suite with a stub body."""
-    if node.parent is not None:
-        if Preview.dummy_implementations in mode and node.parent.type not in (
-            syms.funcdef,
-            syms.async_funcdef,
-            syms.classdef,
-        ):
-            return False
+    if (
+        node.parent is not None
+        and Preview.dummy_implementations in mode
+        and not is_function_or_class(node.parent)
+    ):
+        return False
 
     # If there is a comment, we want to keep it.
     if node.prefix.strip():
