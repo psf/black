006a74c0bc8a639382073cff0f6c96d5e867bdcc
dankrzeminski32 <dankrzeminski32@gmail.com>
2024-02-15 13:08:39 -0600

    undo formatting

--- tests/test_black.py
+++ tests/test_black.py
@@ -483,11 +483,9 @@ class BlackTestCase(BlackBaseTestCase):
 
             return _mocked_calls
 
-        with (
-            patch("pathlib.Path.iterdir", return_value=target_contents),
-            patch("pathlib.Path.resolve", return_value=target_abspath),
-            patch("pathlib.Path.is_dir", side_effect=mock_n_calls([True])),
-        ):
+        with patch("pathlib.Path.iterdir", return_value=target_contents), patch(
+            "pathlib.Path.resolve", return_value=target_abspath
+        ), patch("pathlib.Path.is_dir", side_effect=mock_n_calls([True])):
             # Note that the root folder (project_root) isn't the folder
             # named "root" (aka working_directory)
             report = MagicMock(verbose=True)
@@ -2134,9 +2132,8 @@ class TestCaching:
     @event_loop()
     def test_cache_multiple_files(self) -> None:
         mode = DEFAULT_MODE
-        with (
-            cache_dir() as workspace,
-            patch("concurrent.futures.ProcessPoolExecutor", new=ThreadPoolExecutor),
+        with cache_dir() as workspace, patch(
+            "concurrent.futures.ProcessPoolExecutor", new=ThreadPoolExecutor
         ):
             one = (workspace / "one.py").resolve()
             one.write_text("print('hello')", encoding="utf-8")
@@ -2158,10 +2155,9 @@ class TestCaching:
         with cache_dir() as workspace:
             src = (workspace / "test.py").resolve()
             src.write_text("print('hello')", encoding="utf-8")
-            with (
-                patch.object(black.Cache, "read") as read_cache,
-                patch.object(black.Cache, "write") as write_cache,
-            ):
+            with patch.object(black.Cache, "read") as read_cache, patch.object(
+                black.Cache, "write"
+            ) as write_cache:
                 cmd = [str(src), "--diff"]
                 if color:
                     cmd.append("--color")
@@ -2290,9 +2286,8 @@ class TestCaching:
     @event_loop()
     def test_failed_formatting_does_not_get_cached(self) -> None:
         mode = DEFAULT_MODE
-        with (
-            cache_dir() as workspace,
-            patch("concurrent.futures.ProcessPoolExecutor", new=ThreadPoolExecutor),
+        with cache_dir() as workspace, patch(
+            "concurrent.futures.ProcessPoolExecutor", new=ThreadPoolExecutor
         ):
             failing = (workspace / "failing.py").resolve()
             failing.write_text("not actually python", encoding="utf-8")
