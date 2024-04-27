fa58f076c60abfcf8d38db1f75d00c39ce8bff43
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-04-27 07:41:55 -0700

    Formatting tweaks

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -5,7 +5,7 @@ import sys
 def test_schema_entrypoint() -> None:
     if sys.version_info < (3, 10):
         eps = importlib.metadata.entry_points()["validate_pyproject.tool_schema"]
-        (black_ep,) = (ep for ep in eps if ep.name == "black")
+        (black_ep,) = [ep for ep in eps if ep.name == "black"]
     else:
         (black_ep,) = importlib.metadata.entry_points(
             group="validate_pyproject.tool_schema", name="black"
