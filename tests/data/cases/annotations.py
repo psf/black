f63b8a5723f01e3eaf21466e90fbf896ae42c6c7
MeGaGiGaGon <107241144+megagigagon@users.noreply.github.com>
2024-12-09 14:33:41 -0800

    move test again

---
+++ tests/data/cases/annotations.py
@@ -0,0 +1,17 @@
+# anti 1765 regression
+class Foo:
+    def foo(self):
+        if True:
+            content_ids: Mapping[
+                str, Optional[ContentId]
+            ] = self.publisher_content_store.store_config_contents(files)
+
+# output
+
+# anti 1765 regression
+class Foo:
+    def foo(self):
+        if True:
+            content_ids: Mapping[str, Optional[ContentId]] = (
+                self.publisher_content_store.store_config_contents(files)
+            )
