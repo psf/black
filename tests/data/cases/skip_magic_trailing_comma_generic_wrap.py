7bf7188baa58bfe30aa41bda9d2ffed218e0edd2
hauntsaninja <hauntsaninja@gmail.com>
2025-01-23 19:56:28 -0800

    remove repeated tests

--- tests/data/cases/skip_magic_trailing_comma_generic_wrap.py
+++ tests/data/cases/skip_magic_trailing_comma_generic_wrap.py
@@ -97,17 +97,6 @@ def both_magic_mixed2[T, B,](
 ) -> T:
     return a
 
-def something_something_function[
-    T: Model
-](param: list[int], other_param: type[T], *, some_other_param: bool = True) -> QuerySet[
-    T
-]:
-    pass
-
-
-def func[A_LOT_OF_GENERIC_TYPES: AreBeingDefinedHere, LIKE_THIS, AND_THIS, ANOTHER_ONE, AND_YET_ANOTHER_ONE: ThisOneHasTyping](a: T, b: T, c: T, d: T, e: T, f: T, g: T, h: T, i: T, j: T, k: T, l: T, m: T, n: T, o: T, p: T) -> T:
-    return a
-
 
 # output
 def plain[T, B](a: T, b: T) -> T:
@@ -171,37 +160,4 @@ def both_magic_mixed1[T, B](a: T, b: T) -> T:
 
 
 def both_magic_mixed2[T, B](a: T, b: T) -> T:
-    return a
-
-
-def something_something_function[T: Model](
-    param: list[int], other_param: type[T], *, some_other_param: bool = True
-) -> QuerySet[T]:
-    pass
-
-
-def func[
-    A_LOT_OF_GENERIC_TYPES: AreBeingDefinedHere,
-    LIKE_THIS,
-    AND_THIS,
-    ANOTHER_ONE,
-    AND_YET_ANOTHER_ONE: ThisOneHasTyping,
-](
-    a: T,
-    b: T,
-    c: T,
-    d: T,
-    e: T,
-    f: T,
-    g: T,
-    h: T,
-    i: T,
-    j: T,
-    k: T,
-    l: T,
-    m: T,
-    n: T,
-    o: T,
-    p: T,
-) -> T:
     return a
