844290255f96bbe25c31f21f3f07fd59744998da
hauntsaninja <hauntsaninja@gmail.com>
2025-01-25 04:01:49 -0800

    .

--- src/blackd/__init__.py
+++ src/blackd/__init__.py
@@ -85,7 +85,6 @@ def main(bind_host: str, bind_port: int) -> None:
     web.run_app(app, host=bind_host, port=bind_port, handle_signals=True, print=None)
 
 
-
 @cache
 def executor() -> Executor:
     return ProcessPoolExecutor()
