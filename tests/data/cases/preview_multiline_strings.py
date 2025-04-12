7dfbdac298d43d5dd8b857a230aed4f035c83a97
cobalt <61329810+cobaltt7@users.noreply.github.com>
2025-04-11 20:20:29 -0500

    Keep parens around multiline strings in dicts
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- tests/data/cases/preview_multiline_strings.py
+++ tests/data/cases/preview_multiline_strings.py
@@ -182,6 +182,44 @@ assert some_var == expected_result, f"""
 expected: {expected_result}
 actual: {some_var}"""
 
+
+def foo():
+    a = {
+        xxxx_xxxxxxx.xxxxxx_xxxxxxxxxxxx_xxxxxx_xx_xxx_xxxxxx: {
+            "xxxxx": """Sxxxxx xxxxxxxxxxxx xxx xxxxx (xxxxxx xxx xxxxxxx)""",
+            "xxxxxxxx": (
+                """Sxxxxxxx xxxxxxxx, xxxxxxx xx xxxxxxxxx
+                xxxxxxxxxxxxx xxxxxxx xxxxxxxxx xxx-xxxxxxxxxx xxxxxx xx xxx-xxxxxx"""
+            ),
+            "xxxxxxxx": """Sxxxxxxx xxxxxxxx, xxxxxxx xx xxxxxxxxx
+                xxxxxxxxxxxxx xxxxxxx xxxxxxxxx xxx-xxxxxxxxxx xxxxxx xx xxx-xxxxxx"""
+        },
+    }
+
+
+xxxx_xxxxxxx.xxxxxx_xxxxxxxxxxxx_xxxxxx_xx_xxx_xxxxxx = {
+    "xxxxx": """Sxxxxx xxxxxxxxxxxx xxx xxxxx (xxxxxx xxx xxxxxxx)""",
+    "xxxxxxxx": (
+        """Sxxxxxxx xxxxxxxx, xxxxxxx xx xxxxxxxxx
+    xxxxxxxxxxxxx xxxxxxx xxxxxxxxx xxx-xxxxxxxxxx xxxxxx xx xxx-xxxxxx"""
+    ),
+    "xxxxx_xxxxxxxxxx_xxxxxxxxx_xx": (
+        """
+a
+a
+a
+a
+a"""
+    ),
+    "xx_xxxxx_xxxxxxxxxx_xxxxxxxxx_xx": """
+a
+a
+a
+a
+a""",
+}
+
+
 # output
 """cow
 say""",
@@ -399,3 +437,44 @@ test
 assert some_var == expected_result, f"""
 expected: {expected_result}
 actual: {some_var}"""
+
+
+def foo():
+    a = {
+        xxxx_xxxxxxx.xxxxxx_xxxxxxxxxxxx_xxxxxx_xx_xxx_xxxxxx: {
+            "xxxxx": """Sxxxxx xxxxxxxxxxxx xxx xxxxx (xxxxxx xxx xxxxxxx)""",
+            "xxxxxxxx": (
+                """Sxxxxxxx xxxxxxxx, xxxxxxx xx xxxxxxxxx
+                xxxxxxxxxxxxx xxxxxxx xxxxxxxxx xxx-xxxxxxxxxx xxxxxx xx xxx-xxxxxx"""
+            ),
+            "xxxxxxxx": (
+                """Sxxxxxxx xxxxxxxx, xxxxxxx xx xxxxxxxxx
+                xxxxxxxxxxxxx xxxxxxx xxxxxxxxx xxx-xxxxxxxxxx xxxxxx xx xxx-xxxxxx"""
+            ),
+        },
+    }
+
+
+xxxx_xxxxxxx.xxxxxx_xxxxxxxxxxxx_xxxxxx_xx_xxx_xxxxxx = {
+    "xxxxx": """Sxxxxx xxxxxxxxxxxx xxx xxxxx (xxxxxx xxx xxxxxxx)""",
+    "xxxxxxxx": (
+        """Sxxxxxxx xxxxxxxx, xxxxxxx xx xxxxxxxxx
+    xxxxxxxxxxxxx xxxxxxx xxxxxxxxx xxx-xxxxxxxxxx xxxxxx xx xxx-xxxxxx"""
+    ),
+    "xxxxx_xxxxxxxxxx_xxxxxxxxx_xx": (
+        """
+a
+a
+a
+a
+a"""
+    ),
+    "xx_xxxxx_xxxxxxxxxx_xxxxxxxxx_xx": (
+        """
+a
+a
+a
+a
+a"""
+    ),
+}
