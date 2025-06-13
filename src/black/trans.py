35725c12d70743b804a0497fa9487c8d7ca74711
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-06-12 22:17:34 -0700

    PLR1736 unnecessary-list-index-lookup good change, both speed and clarity improvement

--- src/black/trans.py
+++ src/black/trans.py
@@ -584,7 +584,7 @@ class StringMerger(StringTransformer, CustomSplitMapMixin):
                 <= i
                 < previous_merged_string_idx + previous_merged_num_of_strings
             ):
-                for comment_leaf in line.comments_after(LL[i]):
+                for comment_leaf in line.comments_after(leaf):
                     new_line.append(comment_leaf, preformatted=True)
                 continue
 
