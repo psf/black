c5512d984f15af5aedf319700d5ade68a7567d2b
Ranjodh Singh <ranjodhsingh1729@gmail.com>
2025-08-04 10:21:48 +0530

    Fix function `supports_feature` to return False when `target_versions` is empty.

--- src/black/mode.py
+++ src/black/mode.py
@@ -190,7 +190,9 @@ VERSION_TO_FEATURES: dict[TargetVersion, set[Feature]] = {
 
 
 def supports_feature(target_versions: set[TargetVersion], feature: Feature) -> bool:
-    return all(feature in VERSION_TO_FEATURES[version] for version in target_versions)
+    return bool(target_versions) and all(
+        feature in VERSION_TO_FEATURES[version] for version in target_versions
+    )
 
 
 class Preview(Enum):
