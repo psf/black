14d070fd488c35627673cf67af3d34e013259f44
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-09-18 14:10:22 -0700

    Add explanation for ignores

--- scripts/generate_schema.py
+++ scripts/generate_schema.py
@@ -41,6 +41,8 @@ def generate_schema_from_click(
 
         if param.default is not None and not param.multiple:
             if Version(imp_version("click")) >= Version("8.3.0"):
+                # Ignore the attr-defined error from running with click < 8.3.0, and
+                # also ignore the error for unused-ignore with click >= 8.3.0
                 if param.default is click._utils.UNSET:  # type: ignore[attr-defined, unused-ignore]
                     continue
             result[name]["default"] = param.default
