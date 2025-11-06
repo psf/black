efa26e20ac5694aea8546eac446f03a4e6664d3e
cobalt <61329810+cobaltt7@users.noreply.github.com>
2025-11-05 18:08:09 -0600

    Add tests
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- tests/data/cases/fmtskip11.py
+++ tests/data/cases/fmtskip11.py
@@ -4,3 +4,26 @@ def foo():
 
 # comment 1  # fmt: skip
 # comment 2
+
+v = (
+    foo_dict  # fmt: skip
+    .setdefault("a", {})
+    .setdefault("b", {})
+    .setdefault("c", {})
+    .setdefault("d", {})
+    .setdefault("e", {})
+)
+
+t = (
+    {"foo": "very long string", "bar": "another very long string", "baz": "we should run out of space by now"},  # fmt: skip
+    {"foo": "bar"},
+)
+
+t = (
+    {
+        "foo": "very long string",
+        "bar": "another very long string",
+        "baz": "we should run out of space by now",
+    },  # fmt: skip
+    {"foo": "bar"},
+)
