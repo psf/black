3324121293c1c001365e160908a3b33ac05df0bb
Nidhijha-10 <nidhijha0110@gmail.com>
2025-12-09 18:22:05 +0530

    Fix formatting for Issue #3955

--- src/black/linegen.py
+++ src/black/linegen.py
@@ -572,14 +572,12 @@ class LineGenerator(Visitor[Line]):
                     features=self.features,
                 )
         string_children = [
-            ch for ch in node.children
+            ch 
+            for ch in node.children 
             if isinstance(ch, Leaf) and ch.type == token.STRING
-        ]
-
+              ]
         if len(string_children) > 1:
             wrap_in_parentheses(node, node, visible=True)
-
-
         yield from self.visit_default(node)
 
     def visit_fstring(self, node: Node) -> Iterator[Line]:
