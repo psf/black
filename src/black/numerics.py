053063b6f214a87bc4f035b8100add1cd6be75c8
Samarth D N <dnsam550@gmail.com>
2026-07-31 09:39:37 +0530

    docs: correct normalize_numeric_literal docstring

--- src/black/numerics.py
+++ src/black/numerics.py
@@ -45,7 +45,8 @@ def format_float_or_int_string(text: str) -> str:
 def normalize_numeric_literal(leaf: Leaf) -> None:
     """Normalizes numeric (float, int, and complex) literals.
 
-    All letters used in the representation are normalized to lowercase."""
+    All letters used in the representation are normalized to lowercase,
+    except for hexadecimal digits, which are normalized to uppercase."""
     text = leaf.value.lower()
     if text.startswith(("0o", "0b")):
         # Leave octal and binary literals alone.
