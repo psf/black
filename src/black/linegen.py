7ae0cc3933e417ebb506a6bcadff02a1dfcf539c
Samarth D N <dnsam550@gmail.com>
2026-08-04 08:57:16 +0530

    docs: fix typo in visit_stmt docstring (#5280)

--- src/black/linegen.py
+++ src/black/linegen.py
@@ -217,8 +217,9 @@ class LineGenerator(Visitor[Line]):
         This implementation is shared for `if`, `while`, `for`, `try`, `except`,
         `def`, `with`, `class`, `assert`, and assignments.
 
-        The relevant Python language `keywords` for a given statement will be
-        NAME leaves within it. This methods puts those on a separate line.
+        The relevant Python language `keywords` for a given statement
+        appear as NAME leaves within it. This method puts those on a
+        separate line.
 
         `parens` holds a set of string leaf values immediately after which
         invisible parens should be put.
